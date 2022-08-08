07fb9bdc7effc9de78f4b11d0a1973f201f62a44
cristian <example@gmail.com>
2022-08-08 19:17:19 -0300

    Initial commit

---
+++ src/test/java/ar/edu/undec/nlp/FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.nlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
